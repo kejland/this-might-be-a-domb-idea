1b2df1a209f5dc327d0739d0e2a5087e64fdbe7e
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-03-09 16:17:59 -0500

    upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let para = document.querySelector("p");
+para.style.color = "blue";
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let header = document.querySelector("h1");
+header.style.fontSize = "48px";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = "50%";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "Hi!";
+
+let img = document.querySelector("img");
+img.src = "http://www.tioxic.com/wp-content/uploads/trex_4.jpg";
+img.style.height = "300px";
+
+let img2 = document.querySelector("#image2");
+img2.src = "https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+img2.style.height = "300px";
+
+let item16 = document.querySelector("#item-16");
+item16.innerText = "Won't get fooled again.";
